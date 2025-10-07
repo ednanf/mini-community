07e5a9ce20f5ab729559961fc2503514ea8523a0
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-07 09:17:27 -0300

    feat: implement user registration endpoint and add validation middleware to routes

--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -1,20 +1,41 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import User, { IUserDocument } from '../models/User';
+import { ApiResponse, RegisterUserSuccess } from '../types/api';
 
-const registerUser = (req: Request, res: Response, next: NextFunction) => {
-  res.status(StatusCodes.OK).json({ msg: 'register user hit' });
+const registerUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+    try {
+        const newUser: IUserDocument = await User.create({ email, password });
+        const token = await newUser.createJWT();
+        const response: ApiResponse<RegisterUserSuccess> = {
+            status: 'success',
+            data: {
+                message: 'User registered successfully.',
+                user: newUser.email,
+                token,
+            },
+        };
+        res.status(StatusCodes.CREATED).json(response);
+    } catch (error) {
+        next(error);
+    }
 };
 
 const loginUser = (req: Request, res: Response, next: NextFunction) => {
-  res.status(StatusCodes.OK).json({ msg: 'login user hit' });
+    res.status(StatusCodes.OK).json({ msg: 'login user hit' });
+};
+
+const patchUser = (req: Request, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.OK).json({ msg: 'patch user hit' });
 };
 
 const logoutUser = (req: Request, res: Response, next: NextFunction) => {
-  res.status(StatusCodes.OK).json({ msg: 'logout user hit' });
+    res.status(StatusCodes.OK).json({ msg: 'logout user hit' });
 };
 
 const deleteUser = (req: Request, res: Response, next: NextFunction) => {
-  res.status(StatusCodes.OK).json({ msg: 'delete user hit' });
+    res.status(StatusCodes.OK).json({ msg: 'delete user hit' });
 };
 
-export { registerUser, loginUser, logoutUser, deleteUser };
+export { registerUser, loginUser, patchUser, logoutUser, deleteUser };
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,22 @@
 import express from 'express';
-import { registerUser, loginUser, logoutUser, deleteUser } from '../controllers/usersController';
+import validateWithZod from '../middlewares/validateWithZod';
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    deleteUser,
+    patchUser,
+} from '../controllers/usersController';
+import { userRegisterSchema, userLoginSchema, userPatchSchema } from '../schemas/userSchemas';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+// TODO: Add xss sanitization (dependency)
+// TODO: Add authentication middleware (custom)
+
+router.post('/register', validateWithZod(userRegisterSchema), registerUser);
+router.post('/login', validateWithZod(userLoginSchema), loginUser);
+router.patch('/patch', validateWithZod(userPatchSchema), patchUser);
 router.post('/logout', logoutUser);
 router.delete('/delete', deleteUser);
 
