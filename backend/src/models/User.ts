94dee8ca06a389829719093b063603e682cbf3bf
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-07 09:18:24 -0300

    feat: add bio and avatarURL to the user model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -6,62 +6,81 @@ import comparePasswords from '../utils/comparePasswords';
 
 // TypeScript interface (when just passing user data around)
 export interface IUser {
-  email: string;
-  password: string;
-  createdAt?: Date;
-  updatedAt?: Date;
+    email: string;
+    password: string;
+    avatarUrl?: string;
+    bio?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 // Mongoose document interface (when using the document's methods)
 export interface IUserDocument extends IUser, HydratedDocument<IUser> {
-  comparePassword(candidatePassword: string): Promise<boolean>;
-  createJWT(payload?: Record<string, unknown>): Promise<string>;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+    createJWT(payload?: Record<string, unknown>): Promise<string>;
 }
 
 const userSchema = new Schema<IUserDocument>(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      minlength: [5, 'Email must be at least 5 characters long.'],
-      validate: {
-        validator(email: string) {
-          return validator.isEmail(email);
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            minlength: [5, 'Email must be at least 5 characters long.'],
+            validate: {
+                validator(email: string) {
+                    return validator.isEmail(email);
+                },
+                message: 'Please provide a valid email address.',
+            },
         },
-        message: 'Please provide a valid email address.',
-      },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: [6, 'Password must be at least 6 characters long.'],
+        },
+        avatarUrl: {
+            type: String,
+            required: false,
+            trim: true,
+            maxlength: [2048, 'URL must contain 2048 characters or less.'],
+            validate: {
+                validator(url: string) {
+                    return validator.isURL(url);
+                },
+            },
+        },
+        bio: {
+            type: String,
+            required: false,
+            trim: true,
+            maxlength: [280, 'Bio must contain 280 characters or less.'],
+        },
     },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: [6, 'Password must be at least 6 characters long.'],
-    },
-  },
-  { timestamps: true },
+    { timestamps: true },
 );
 
 // Hash password only if it was modified
 userSchema.pre('save', async function hashPasswordBeforeSave() {
-  if (this.isModified('password')) {
-    this.password = await hashPassword(this.password);
-  }
+    if (this.isModified('password')) {
+        this.password = await hashPassword(this.password);
+    }
 });
 
 // User schema methods
 userSchema.methods.createJWT = async function createUserJWT(payload: Record<string, unknown> = {}) {
-  // Package the user's MongoDB _id as userId into the JWT payload.
-  // This is  where the userId is embedded into the token.
-  return createJWT({ userId: this._id, ...payload });
+    // Package the user's MongoDB _id as userId into the JWT payload.
+    // This is  where the userId is embedded into the token.
+    return createJWT({ userId: this._id, ...payload });
 };
 
 userSchema.methods.comparePassword = async function compareUserPassword(
-  candidatePassword: string,
+    candidatePassword: string,
 ): Promise<boolean> {
-  return comparePasswords(candidatePassword, this.password);
+    return comparePasswords(candidatePassword, this.password);
 };
 
 /**
