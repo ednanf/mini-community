193b991bde8fad23f5fbe24b30c127372873f296
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-07 08:50:00 -0300

    feat: add errorhandler middleware to the server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 
 import userRoutes from './routes/userRoutes';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
@@ -11,5 +12,9 @@ app.use(morgan('tiny'));
 
 // Routes
 app.use('/api/v1/user', userRoutes);
+
+// Errors
+app.use(errorHandler);
+// TODO: add 404 handler
 
 export default app;
