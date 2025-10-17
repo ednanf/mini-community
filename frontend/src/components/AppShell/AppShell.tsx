c6a0954970252beed5c5f9a62adf40a414d5a1fc
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-17 09:58:55 -0300

    feat: enhance AppShell layout with CSS styling and NavBar integration
    - Added layout styles in `AppShell.module.css` for better structure.
    - Updated `AppShell.tsx` to include a header and integrated `NavBar`.
    - Defined background color variables in `index.css` for light and dark themes.

--- frontend/src/components/AppShell/AppShell.tsx
+++ frontend/src/components/AppShell/AppShell.tsx
@@ -1,12 +1,20 @@
 import { Outlet } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar.tsx';
 import styles from './AppShell.module.css';
 
 const AppShell = () => {
   return (
-    <>
-      <p>NavBar</p>
-      <Outlet />
-    </>
+    <div className={styles.layout}>
+      <header>
+        <h1>App Header</h1>
+      </header>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <nav className={styles.nav}>
+        <NavBar />
+      </nav>
+    </div>
   );
 };
 export default AppShell;
