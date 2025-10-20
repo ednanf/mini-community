49e386cb688c9cc51887237faca5c588dadf0201
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-20 11:06:15 -0300

    feat(AppShell): conditionally render header and nav based on pathname
    * Updated the AppShell component to only display the header and navigation when the pathname is not the root ('/').
    * This improves the user experience by avoiding unnecessary UI elements on the home page.

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,4 +1,24 @@
+import { Link } from 'react-router-dom';
+import { VStack } from '../../components/Layout/VStack.tsx';
+import PillButtonLink from '../../components/PillButtonLink/PillButtonLink.tsx';
+import logo from '../../assets/logo-no-bg.png';
+import styles from './LandingPage.module.css';
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <VStack justify={'center'} align={'center'} gap={'lg'} textAlign={'center'} margin={'sm'}>
+      <img src={logo} alt="Logo" className={styles.logo} />
+      <h2>Welcome to Mini Community! </h2>
+      <p>Your go-to platform for connecting with friends and sharing moments.</p>
+      <p>Join us today and start your journey!</p>
+      <VStack justify={'center'} align={'center'} gap={'sm'} margin={'lg'}>
+        <PillButtonLink to={'login'}>Get Started</PillButtonLink>
+        <p>or</p>
+        <Link to="/login" className={styles.loginLink}>
+          Log In
+        </Link>
+      </VStack>
+    </VStack>
+  );
 };
 export default LandingPage;
