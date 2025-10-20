6fe64b3ef99445e168a83ec9ad7eb145ff419680
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-20 10:10:03 -0300

    feat(Header): add styling for header and back button integration
    - Introduced CSS styles for the header and back button components.
    - Enhanced layout with flexbox for better alignment and responsiveness.
    - Ensured backdrop-filter for a modern UI effect.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { HStack } from '../Layout/HStack.tsx';
 import BackButton from '../BackButton/BackButton.tsx';
 
+import styles from './Header.module.css';
+
 interface HeaderProps {
   backButton: boolean;
   children: React.ReactNode;
 }
 
+// TODO: move the back button to the left side and ensure the title is centered
+
 const Header = ({ backButton, children }: HeaderProps) => {
   return (
-    <HStack>
-      {backButton ? <BackButton /> : ''}
-      {children}
+    <HStack align={'center'} justify={'center'} className={styles.header}>
+      {backButton ? (
+        <span className={styles.backButtonWrapper}>
+          <BackButton />
+        </span>
+      ) : (
+        ''
+      )}
+      <span className={styles.children}>{children}</span>
     </HStack>
   );
 };
