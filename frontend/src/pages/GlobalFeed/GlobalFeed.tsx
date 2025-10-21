bddff0b69f5407467d461edbdd9cb2294a3b1ae6
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-21 09:17:32 -0300

    feat(GlobalFeed): add PostData type definition for improved type safety
    * Introduced a new type `PostData` to define the structure of post objects.
    * This enhances type safety and clarity in the `GlobalFeed` component.

--- frontend/src/pages/GlobalFeed/GlobalFeed.tsx
+++ frontend/src/pages/GlobalFeed/GlobalFeed.tsx
@@ -1,4 +1,11 @@
+type PostData = {
+    createdBy: '68e66454442abc1da09be66f';
+    content: 'Test 15';
+    createdAt: '2025-10-14T13:20:20.494Z';
+    updatedAt: '2025-10-14T13:20:20.494Z';
+};
+
 const GlobalFeed = () => {
-  return <div>GlobalFeed</div>;
+    return <div>GlobalFeed</div>;
 };
 export default GlobalFeed;
