e5fef5322efbb5585e2b30977b390eb7cb1cca7c
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-22 09:48:39 -0300

    feat(corsOptions): add additional allowed origin for CORS configuration
    * Expanded the `allowedOrigins` array to include 'http://10.0.0.102:5173' for local development.
    * This change allows requests from another local environment, improving flexibility during testing.

--- backend/src/configs/corsOptions.ts
+++ backend/src/configs/corsOptions.ts
@@ -1,6 +1,6 @@
 // This file configures CORS options for the application.
 // It allows requests from specific origins and defines allowed HTTP methods.
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'http://10.0.0.102:5173'];
 
 const corsOptions = {
     origin: allowedOrigins,
