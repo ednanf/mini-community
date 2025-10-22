cd62eb6a7e48d93c649dccb9bc77b4f86df9936d
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-10-22 10:57:57 -0300

    feat(PostCard): enhance PostCard component with date formatting and layout adjustments
    * Updated border-radius for card styling.
    * Changed margin logic for the last child in card body.
    * Integrated `mongodbDateFormatter` for date display.
    * Updated props to include `nickname`, `postBody`, and `date`.

--- frontend/src/components/PostCard/PostCard.tsx
+++ frontend/src/components/PostCard/PostCard.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import styles from './PostCard.module.css';
 import { VStack } from '../Layout/VStack.tsx';
 import { HStack } from '../Layout/HStack.tsx';
+import mongodbDateFormatter from '../../utils/mongodbDateFormatter.ts';
+import styles from './PostCard.module.css';
 
-const PostCard = ({ image }) => {
+interface PostCardProps {
+    image: string;
+    nickname: string;
+    postBody: string;
+    date: string;
+}
+
+const PostCard = ({ image, nickname, postBody, date }: PostCardProps) => {
+    const formattedDate = mongodbDateFormatter(date);
+
     return (
         <div className={styles.cardBody}>
             <HStack padding={'sm'} gap={'md'}>
@@ -13,10 +22,10 @@ const PostCard = ({ image }) => {
                     className={styles.profileImage}
                 />
                 <VStack>
-                    <p className={styles.nickname}>@test</p>
-                    <p>My balls are itching</p>
+                    <p className={styles.nickname}>@{nickname}</p>
+                    <p>{postBody}</p>
                     <HStack>
-                        <p className={styles.timestamp}>21 Oct 2025, 13:20</p>
+                        <p className={styles.timestamp}>{formattedDate}</p>
                     </HStack>
                 </VStack>
             </HStack>
