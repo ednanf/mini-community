0b66a2ef7d157cb5ff45a4a70bb29c79e56a619a
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-11-11 09:38:10 -0300

    feat(pwa): integrate VitePWA for progressive web app support
    - Added VitePWA plugin to enhance PWA capabilities.
    - Configured manifest with app details and icons.
    - Updated theme color in index.html for better branding.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Mini Community',
+                short_name: 'MiniCom',
+                description: 'A mini social media application for fun.',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'favicon/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'favicon/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
+});
