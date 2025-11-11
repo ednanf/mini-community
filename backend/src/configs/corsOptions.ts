6ba19e999f115ff7f5089127d1c42b5ffa92e2d3
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-11-11 13:17:46 -0300

    chore(corsOptions): add additional allowed origins for CORS
    - Included new origins:
      - `https://mini-community.frizzera.dev`
      - `https://mini-community-ednan.vercel.app`
    - This change allows requests from these new domains to enhance accessibility.

--- backend/src/configs/corsOptions.ts
+++ backend/src/configs/corsOptions.ts
@@ -1,6 +1,11 @@
 // This file configures CORS options for the application.
 // It allows requests from specific origins and defines allowed HTTP methods.
-const allowedOrigins = ['http://localhost:5173', 'http://10.0.0.102:5173'];
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://10.0.0.102:5173',
+    'https://mini-community.frizzera.dev',
+    'https://mini-community-ednan.vercel.app',
+];
 
 const corsOptions = {
     origin: allowedOrigins,
